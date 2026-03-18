b3647c8c58f244a0372e18caf3a6252d4b5ae89a
tejapratapkdr007 <tejapratapkdr007@gmail.com>
2026-03-18 14:32:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
+app.use(express.urlencoded({ extended: true, l
 // =====================================================
 // DATA FILE PERSISTENCE (survives restarts)
 // =====================================================
